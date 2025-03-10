599c40d00b3d958180f01a5a92859dd9e9f6d663
biple <rukatozukato@gmail.com>
2025-03-10 17:05:34 +0545

    simple-js

---
+++ JS/Video54/script.js
@@ -0,0 +1,16 @@
+alert("Hello World");
+
+console.log("Code is running...");
+
+var a = prompt("Enter a number");
+var isTrue = confirm("Are you sure you want to leave the page and burn your computer?");
+
+if (isTrue){
+    console.log("The computer is burning...");
+}
+
+else{
+    console.log("The computer is not burning...");
+}
+
+console.log("The number is: " + a);
